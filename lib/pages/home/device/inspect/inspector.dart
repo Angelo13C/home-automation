e7a79e4758d7a8874156061b01d26f6a72ce9f3d
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-06-04 11:11:51 +0200

    ✨ Add a page that lets you inspect a device

--- lib/pages/home/device/inspect/inspector.dart
+++ lib/pages/home/device/inspect/inspector.dart
@@ -1,3 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:home_automation/pages/home/device/data/device.dart';
+
+class DeviceInspectorPage extends StatefulWidget {
+  final Device device;
+
+  const DeviceInspectorPage(this.device, {super.key});
+
+  @override
+  State<DeviceInspectorPage> createState() => _DeviceInspectorPageState();
+}
+
+class _DeviceInspectorPageState extends State<DeviceInspectorPage> {
+  @override
+  Widget build(BuildContext context) {
+    final deviceName = widget.device.name;
+    return Scaffold(
+      body: Builder(
+        builder: (context) => widget.device.type.inspect(
+          InspectorParameters(
+            appBar: AppBar(
+              backgroundColor: const Color(0xFF262626),
+              title: Text(
+                deviceName,
+                style: const TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              automaticallyImplyLeading: false,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class InspectorParameters {
   AppBar appBar;
 
